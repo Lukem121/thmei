97103375e577e5a9dbf54262282da66225c791e2
Lukem121 <lukeask@hotmail.co.uk>
2021-03-24 16:37:43 +0000

    cta added

--- src/sections/Main.svelte
+++ src/sections/Main.svelte
@@ -1,6 +1,5 @@
 <script>
     import devs from '../assets/img1.png';
-
 </script>
 
 <main class="container md:mx-auto mt-6 px-6">
@@ -11,4 +10,10 @@
     </h1>
     <h2 class="text-2xl font-medium mt-4">Not the average website, not the average team, not the average products</h2>
     <img src={devs} class="mt-6" alt="">
+    <div class="flex justify-center">
+        <button class="inline-flex justify-center items-center space-x-2 py-3 px-6 mt-6 bg-indigo-500 text-white focus:outline-none rounded">
+            <p>Get in contact</p>
+            <i class="far fa-envelope"></i>
+        </button>
+    </div>
 </main>
