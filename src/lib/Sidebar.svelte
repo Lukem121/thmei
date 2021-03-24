dbb1621deb940870aec3d111a12058e73e8c05f4
Lukem121 <lukeask@hotmail.co.uk>
2021-03-24 00:51:09 +0000

    smooth sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,18 +1,27 @@
 <script>
+	import * as animateScroll from "svelte-scrollto";
+
 	export let open = false
+
+	const navigate = (val) => {
+		open = !open;
+		animateScroll.scrollTo({element: val, delay: 350})
+	}
 </script>
 
 <aside class="absolute w-full h-full z-10 bg-white border-r-2 shadow-lg" class:open>
 	<nav class="flex flex-col justify-center items-center p-12 text-xl">
-		<a class="block" href="#about">About</a>
-		<a class="block" href="#contact">Contact</a>
+		<button on:click={() => navigate("#about")} class="mr-5 hover:text-gray-900">About</button>
+        <button on:click={() => navigate("#pricing")} class="mr-5 hover:text-gray-900">Pricing</button>
+        <button on:click={() => navigate("#services")} class="mr-5 hover:text-gray-900">Services</button>
+        <button on:click={() => navigate("#testimonials")} class="mr-5 hover:text-gray-900">Testimonials</button>
 	</nav>
 </aside>
 
 <style>
 	aside {
 		left: -100%;
-		transition: left 0.3s ease-in-out
+		transition: left 0.3s ease-in
 	}
 	
 	.open {
